cdd31df67fd24d74c102ee8eeb21efbe586495f6
D-Archi <d@a.com>
2019-04-24 17:18:34 +0900

    4/24 commit

--- src/main/java/io/dongsuk/dbatch/common/controller/MainController.java
+++ src/main/java/io/dongsuk/dbatch/common/controller/MainController.java
@@ -1,4 +1,4 @@
-package io.dongsuk.dbatch.common;
+package io.dongsuk.dbatch.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
