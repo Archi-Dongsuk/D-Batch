9ef7e67966b1e1c5be7056275d92c382e4355a1c
D-Archi <d@a.com>
2019-06-10 16:02:38 +0900

    jobstore jdbc 테스트 확인

--- src/main/java/io/dongsuk/dbatch/common/batch/TestCronTrigger.java
+++ src/main/java/io/dongsuk/dbatch/common/batch/TestCronTrigger.java
@@ -4,19 +4,27 @@ import io.dongsuk.dbatch.job.sample.testJob;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TestCronTrigger {
+
+    @Autowired
+    private SchedulerFactoryBean schedulerFactoryBean;
+
     private SchedulerFactory schedulerFactory;
     private Scheduler scheduler;
 
     public void createScheduler() throws SchedulerException {
         schedulerFactory = new StdSchedulerFactory();
-        scheduler = schedulerFactory.getScheduler();
-        scheduler.start();
+        //scheduler = schedulerFactory.getScheduler();
+        scheduler = schedulerFactoryBean.getScheduler();
 
         JobDetail job = JobBuilder.newJob(testJob.class).withIdentity("testJob").build();
+
 
         Trigger trigger = TriggerBuilder.newTrigger().withSchedule(CronScheduleBuilder.cronSchedule("15 * * * * ?")).build();
 
